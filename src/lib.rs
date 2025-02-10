b71dc863e0f234e4d06c45d2c4ab9191f1c6fdf8
Stephane Raux <stephaneyfx@gmail.com>
2025-02-09 22:23:43 -0600

    Update documentation

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@
 //! Base64 adapter for `#[serde(with = ...)]`. See [`serdapt`](https://docs.rs/serdapt) for more
 //! information on how to use such adapters.
 //!
+//! The documentation for [`Base64`] and [`Base64Array`] provides examples.
+//!
 //! # Contribute
 //! All contributions shall be licensed under the [0BSD license](https://spdx.org/licenses/0BSD.html).
 
